2f1f57c33c7f7af9e00081b0df729874f8c98539
Antony Shchukin <szwtdy@gmail.com>
2018-09-12 15:34:56 +0300

    wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,25 +11,23 @@ import {Address} from "./components/address/Address";
 import {Header} from "./components/header/Header";
 import Grid from "@material-ui/core/Grid/Grid";
 
-
 class App extends React.Component {
+
     public render() {
         return (
-            <div className="App">
-                <Router>
-                    <div>
-                        <Header/>
-                        <Grid container direction="column" justify="flex-start" alignItems="center">
-                            <Route exact path="/" component={Home}/>
-                            <Route path="/blocks" component={Blocks}/>
-                            <Route path="/block/:blockHash" component={Block}/>
-                            <Route path="/transactions" component={Transactions}/>
-                            <Route path="/transaction/:txHash" component={Transaction}/>
-                            <Route path="/address/:address" component={Address}/>
-                        </Grid>
-                    </div>
-                </Router>
-            </div>
+            <Router forceRefresh={true}>
+                <div className="App">
+                    <Header/>
+                    <Grid container direction="column" justify="flex-start" alignItems="center">
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/blocks" component={Blocks}/>
+                        <Route path="/block/:blockHash" component={Block}/>
+                        <Route path="/transactions" component={Transactions}/>
+                        <Route path="/transaction/:txHash" component={Transaction}/>
+                        <Route path="/address/:address" component={Address}/>
+                    </Grid>
+                </div>
+            </Router>
         );
     }
 }
--- frontend/src/components/block/Block.tsx
+++ frontend/src/components/block/Block.tsx
@@ -23,12 +23,9 @@ export class Block extends React.Component<any, BlockState> {
         loading: false
     } as BlockState;
 
+
     componentDidMount() {
-        this.setState({
-            blockHash: this.props.match.params.blockHash
-        } as BlockState);
-        let url = "http://localhost:3544/blocks?hash=eq." + this.state.blockHash;
-        console.log(url);
+        let url = "http://localhost:3544/blocks?hash=eq." + this.props.match.params.blockHash;
         fetch(url)
             .then(response => {
                 if (!response.ok) {
@@ -37,13 +34,14 @@ export class Block extends React.Component<any, BlockState> {
                 return response.json()
             })
             .then(result => {
-                if (result.length == 0){
+                console.log("loaded");
+                if (result.length == 0) {
                     throw new Error("block doesn't exist")
                 }
                 this.setState({
                     loading: true,
                     block: result[0]
-                } as BlockState)
+                } as BlockState);
             })
             .catch(err => {
                 this.setState({
@@ -56,61 +54,47 @@ export class Block extends React.Component<any, BlockState> {
 
     render() {
 
-        if (this.state.error != null){
-            console.log(this.state.error)
+        if (this.state.error != null) {
+            return (
+                <h1>error - {this.state.error}</h1>
+            )
         }
 
         return (
             <Paper>
-                <h1>Block</h1>
+                <h1>Block #{this.state.block.number}</h1>
 
-                <Grid container spacing={24}>
-                    <Grid item xs>
-                        <Grid item xs={4}>Hash</Grid>
-                        <Grid item xs={6}>{this.state.block.hash}</Grid>
+                <Grid container spacing={8}>
+                    <Grid item xs={2}>Hash</Grid>
+                    <Grid item xs={10}>{this.state.block.hash}</Grid>
+
+                    <Grid item xs={2}>Timestamp</Grid>
+                    <Grid item xs={10}>{this.state.block.timestamp}</Grid>
+
+                    <Grid item xs={2}>Transactions</Grid>
+                    <Grid item xs={10}>{this.state.block.txCount}</Grid>
+
+                    <Grid item xs={2}>Parent Hash</Grid>
+                    <Grid item xs={10}>
+                        <Link replace={true} to={"/block/" + this.state.block.parentHash}>
+                            {this.state.block.parentHash}
+                        </Link>
                     </Grid>
 
-                    <Grid item xs>
-                        <Grid item xs={4}>Timestamp</Grid>
-                        <Grid item xs={6}>{this.state.block.timestamp}</Grid>
-                    </Grid>
+                    <Grid item xs={2}>Gas Used</Grid>
+                    <Grid item xs={10}>{this.state.block.gasUsed}</Grid>
 
-                    <Grid item xs>
-                        <Grid item xs={4}>Transactions</Grid>
-                        <Grid item xs={6}>{this.state.block.txCount}</Grid>
-                    </Grid>
+                    <Grid item xs={2}>Gas Limit</Grid>
+                    <Grid item xs={10}>{this.state.block.gasLimit}</Grid>
 
-                    <Grid item xs>
-                        <Grid item xs={4}>Parent Hash</Grid>
-                        <Grid item xs={6}>
-                            <Link to={"/block/"+this.state.block.parentHash}>{this.state.block.parentHash}</Link>
-                        </Grid>
-                    </Grid>
+                    <Grid item xs={2}>Mined by</Grid>
+                    <Grid item xs={10}>{this.state.block.miner}</Grid>
 
-                    <Grid item xs>
-                        <Grid item xs={4}>Gas Used</Grid>
-                        <Grid item xs={6}>{this.state.block.gasUsed}</Grid>
-                    </Grid>
+                    <Grid item xs={2}>Nonce</Grid>
+                    <Grid item xs={10}>{this.state.block.nonce}</Grid>
 
-                    <Grid item xs>
-                        <Grid item xs={4}>Gas Limit</Grid>
-                        <Grid item xs={6}>{this.state.block.gasLimit}</Grid>
-                    </Grid>
-
-                    <Grid item xs>
-                        <Grid item xs={4}>Mined by</Grid>
-                        <Grid item xs={6}>{this.state.block.miner}</Grid>
-                    </Grid>
-
-                    <Grid item xs>
-                        <Grid item xs={4}>Nonce</Grid>
-                        <Grid item xs={6}>{this.state.block.nonce}</Grid>
-                    </Grid>
-
-                    <Grid item xs>
-                        <Grid item xs={4}>Size</Grid>
-                        <Grid item xs={6}>{this.state.block.size}</Grid>
-                    </Grid>
+                    <Grid item xs={2}>Size</Grid>
+                    <Grid item xs={10}>{this.state.block.size}</Grid>
                 </Grid>
             </Paper>
         )
