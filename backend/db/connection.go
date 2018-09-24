ac5bf30eb39f0bcd45f37bf0db02ea3f13ba3f7b
Antony Shchukin <szwtdy@gmail.com>
2018-09-24 20:05:54 +0300

    [back] ref: db incapsulation

--- backend/db/connection.go
+++ backend/db/connection.go
@@ -3,7 +3,11 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
 	_ "github.com/lib/pq"
+	"github.com/sonm-io/explorer/backend/types"
+	"log"
+	"math/big"
 )
 
 type Connection struct {
@@ -28,14 +32,166 @@ func NewConnection(cfg *Config) (*Connection, error) {
 	}, nil
 }
 
-func (c *Connection) DBConnection() *sql.DB {
-	return c.db
+func (conn *Connection) DBConnection() *sql.DB {
+	return conn.db
 }
 
-func (c *Connection) NewTransaction() (*sql.Tx, error) {
-	return c.db.Begin()
+func (conn *Connection) NewTransaction() (*sql.Tx, error) {
+	return conn.db.Begin()
 }
 
-func (c *Connection) Close() error {
-	return c.db.Close()
+func (conn *Connection) Close() error {
+	return conn.db.Close()
+}
+
+func (conn *Connection) GetBestBlock() (uint64, error) {
+	rows, err := conn.db.Query(`SELECT coalesce(max(number), 0) AS bestBlock FROM blocks;`)
+	if err != nil {
+		return 0, err
+	}
+	defer rows.Close()
+
+	var bestBlock uint64
+	rows.Next()
+	err = rows.Scan(&bestBlock)
+	if err != nil {
+		return 0, err
+	}
+	return bestBlock, nil
+}
+
+func (conn *Connection) GetUnfilledIntervals() ([]types.Interval, error) {
+	rows, err := conn.db.Query(`
+		SELECT number + 1 as start_interval, next_id - 1 as finish_interval
+		FROM (SELECT number, LEAD(number)OVER (ORDER BY number) AS next_id FROM blocks)T
+		WHERE number + 1 <> next_id
+		LIMIT 10000
+	`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var s []types.Interval
+
+	for rows.Next() {
+		var t types.Interval
+		err := rows.Scan(&t.Start, &t.Finish)
+		if err != nil {
+			return nil, err
+		}
+		s = append(s, t)
+	}
+
+	if rows.Err() != nil {
+		return nil, rows.Err()
+	}
+
+	return s, nil
+}
+
+func (conn *Connection) SaveBlock(block *types.Block) error {
+	t, err := conn.NewTransaction()
+	if err != nil {
+		return fmt.Errorf("failed to begin transaction: %s", err)
+	}
+
+	bloom := common.Bytes2Hex(block.Block.Bloom().Bytes())
+
+	size := big.NewInt(0).SetUint64(uint64(block.Block.Size()))
+
+	extra := common.Bytes2Hex(block.Block.Extra())
+
+	_, err = t.Exec(`INSERT INTO blocks(
+			number,
+			hash,
+			"parentHash",
+			nonce,
+			"sha3Uncles",
+			"logsBloom",
+			"transactionsRoot",
+			"stateRoot",
+			"receiptsRoot",
+			miner,
+			difficulty,
+			"totalDifficulty",
+			size,
+			"extraData",
+			"gasLimit",
+			"gasUsed",
+			timestamp,
+			"mixhash",
+			"txCount"
+			) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)`,
+		block.Block.NumberU64(),
+		block.Block.Hash().String(),
+		block.Block.ParentHash().String(),
+		block.Block.Nonce(),
+		block.Block.UncleHash().String(),
+		bloom,
+		block.Block.TxHash().String(),
+		block.Block.Root().String(),
+		block.Block.ReceiptHash().String(),
+		block.Block.Coinbase().String(),
+		block.Block.Difficulty().Uint64(),
+		0, // TODO: add total difficulty field
+		size.Uint64(),
+		extra,
+		block.Block.GasLimit(),
+		block.Block.GasUsed(),
+		block.Block.Time().Uint64(),
+		block.Block.MixDigest().String(),
+		len(block.Transactions))
+	if err != nil {
+		log.Printf("error while inserting block: %s", err)
+		return t.Rollback()
+	}
+	for _, receipt := range block.Transactions {
+		tx := block.Block.Transaction(receipt.TxHash)
+
+		v, r, s := tx.RawSignatureValues()
+
+		data := common.Bytes2Hex(tx.Data())
+
+		_, err := t.Exec(
+			`INSERT INTO transactions(
+				hash,
+				nonce ,
+				"blockHash",
+				"blockNumber",
+				"transactionIndex",
+				"from",
+				"to",
+				"value",
+				gas,
+				"gasPrice",
+				input,
+				v,
+				r,
+				s, 
+				status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)`,
+			tx.Hash().String(),
+			tx.Nonce(),
+			block.Block.Hash().String(),
+			block.Block.NumberU64(),
+			receipt.TransactionIndex,
+			receipt.From.String(),
+			tx.To().String(),
+			tx.Value().Uint64(),
+			tx.Gas(),
+			tx.GasPrice().Uint64(),
+			data,
+			v.String(),
+			r.String(),
+			s.String(),
+			receipt.Status)
+		if err != nil {
+			log.Printf("error while inserting transaction: %s", err)
+			return t.Rollback()
+		}
+	}
+	err = t.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- backend/filler/filler.go
+++ backend/filler/filler.go
@@ -2,8 +2,6 @@ package filler
 
 import (
 	"context"
-	"fmt"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/sonm-io/core/blockchain"
 	"github.com/sonm-io/explorer/backend/db"
 	"github.com/sonm-io/explorer/backend/types"
@@ -39,7 +37,7 @@ func (f *Filler) Start(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	bestBlockNumber, err := f.GetBestBlock()
+	bestBlockNumber, err := f.db.GetBestBlock()
 	if err != nil {
 		return err
 	}
@@ -65,7 +63,7 @@ func (f *Filler) Start(ctx context.Context) error {
 	for {
 		select {
 		case <-t10sec.C:
-			intervals, err := f.GetUnfilledIntervals()
+			intervals, err := f.db.GetUnfilledIntervals()
 			if err != nil {
 				return err
 			}
@@ -83,7 +81,7 @@ func (f *Filler) Start(ctx context.Context) error {
 				if err != nil {
 					return
 				}
-				bestBlockNumber, err := f.GetBestBlock()
+				bestBlockNumber, err := f.db.GetBestBlock()
 				if err != nil {
 					return
 				}
@@ -112,7 +110,7 @@ func (f *Filler) Start(ctx context.Context) error {
 		case block := <-f.saveChan:
 			dbPool <- 1
 			go func() {
-				err = f.saveBlock(block)
+				err = f.db.SaveBlock(block)
 				if err != nil {
 					log.Println(err)
 					return
@@ -139,158 +137,4 @@ func (f *Filler) fillBlock(ctx context.Context, number *big.Int) (*types.Block, 
 		result.Transactions = append(result.Transactions, rec)
 	}
 	return result, nil
-}
-
-func (f *Filler) saveBlock(block *types.Block) error {
-	t, err := f.db.NewTransaction()
-	if err != nil {
-		return fmt.Errorf("failed to begin transaction: %s", err)
-	}
-
-	bloom := common.Bytes2Hex(block.Block.Bloom().Bytes())
-
-	size := big.NewInt(0).SetUint64(uint64(block.Block.Size()))
-
-	extra := common.Bytes2Hex(block.Block.Extra())
-
-	_, err = t.Exec(`INSERT INTO blocks(
-			number,
-			hash,
-			"parentHash",
-			nonce,
-			"sha3Uncles",
-			"logsBloom",
-			"transactionsRoot",
-			"stateRoot",
-			"receiptsRoot",
-			miner,
-			difficulty,
-			"totalDifficulty",
-			size,
-			"extraData",
-			"gasLimit",
-			"gasUsed",
-			timestamp,
-			"mixhash",
-			"txCount"
-			) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)`,
-		block.Block.NumberU64(),
-		block.Block.Hash().String(),
-		block.Block.ParentHash().String(),
-		block.Block.Nonce(),
-		block.Block.UncleHash().String(),
-		bloom,
-		block.Block.TxHash().String(),
-		block.Block.Root().String(),
-		block.Block.ReceiptHash().String(),
-		block.Block.Coinbase().String(),
-		block.Block.Difficulty().Uint64(),
-		0, // TODO: add total difficulty field
-		size.Uint64(),
-		extra,
-		block.Block.GasLimit(),
-		block.Block.GasUsed(),
-		block.Block.Time().Uint64(),
-		block.Block.MixDigest().String(),
-		len(block.Transactions))
-	if err != nil {
-		log.Printf("error while inserting block: %s", err)
-		return t.Rollback()
-	}
-	for _, receipt := range block.Transactions {
-		tx := block.Block.Transaction(receipt.TxHash)
-
-		v, r, s := tx.RawSignatureValues()
-
-		data := common.Bytes2Hex(tx.Data())
-
-		_, err := t.Exec(
-			`INSERT INTO transactions(
-				hash,
-				nonce ,
-				"blockHash",
-				"blockNumber",
-				"transactionIndex",
-				"from",
-				"to",
-				"value",
-				gas,
-				"gasPrice",
-				input,
-				v,
-				r,
-				s, 
-				status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)`,
-			tx.Hash().String(),
-			tx.Nonce(),
-			block.Block.Hash().String(),
-			block.Block.NumberU64(),
-			receipt.TransactionIndex,
-			receipt.From.String(),
-			tx.To().String(),
-			tx.Value().Uint64(),
-			tx.Gas(),
-			tx.GasPrice().Uint64(),
-			data,
-			v.String(),
-			r.String(),
-			s.String(),
-			receipt.Status)
-		if err != nil {
-			log.Printf("error while inserting transaction: %s", err)
-			return t.Rollback()
-		}
-	}
-	err = t.Commit()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (f *Filler) GetBestBlock() (uint64, error) {
-	conn := f.db.DBConnection()
-	rows, err := conn.Query(`SELECT coalesce(max(number), 0) AS bestBlock FROM blocks;`)
-	if err != nil {
-		return 0, err
-	}
-	defer rows.Close()
-
-	var bestBlock uint64
-	rows.Next()
-	err = rows.Scan(&bestBlock)
-	if err != nil {
-		return 0, err
-	}
-	return bestBlock, nil
-}
-
-func (f *Filler) GetUnfilledIntervals() ([]types.Interval, error) {
-	conn := f.db.DBConnection()
-	rows, err := conn.Query(`
-		SELECT number + 1 as start_interval, next_id - 1 as finish_interval
-		FROM (SELECT number, LEAD(number)OVER (ORDER BY number) AS next_id FROM blocks)T
-		WHERE number + 1 <> next_id
-		LIMIT 10000
-	`)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-	var s []types.Interval
-
-	for rows.Next() {
-		var t types.Interval
-		err := rows.Scan(&t.Start, &t.Finish)
-		if err != nil {
-			return nil, err
-		}
-		s = append(s, t)
-	}
-
-	if rows.Err() != nil {
-		return nil, rows.Err()
-	}
-
-	return s, nil
 }
