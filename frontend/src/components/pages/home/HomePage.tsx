6a7d7e81dd5926ecf2e2bc0fbb529cb7e14537e2
Antony Shchukin <szwtdy@gmail.com>
2018-09-24 13:54:49 +0300

    wip

--- frontend/src/components/pages/home/HomePage.tsx
+++ frontend/src/components/pages/home/HomePage.tsx
@@ -1,11 +1,26 @@
 import * as React from "react";
 import Grid from "@material-ui/core/Grid/Grid";
+import SearchBar from "./parts/SearchBar";
 
 
 class HomePage extends React.Component {
     render() {
         return (
-            <Grid container>
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: '100vh' }}
+            >
+
+                <Grid item xs={3}>
+                    <h1>SONM SE</h1>
+                    <SearchBar classes={{
+
+                    }}/>
+                </Grid>
 
             </Grid>
         )
