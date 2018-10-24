c6df49d975b471f9d415dd1dc83529f2a8c5b02a
Antony Shchukin <szwtdy@gmail.com>
2018-10-24 18:25:56 +0300

    [front] feat: pagination on address page

--- frontend/src/components/pages/address/AddressPage.tsx
+++ frontend/src/components/pages/address/AddressPage.tsx
@@ -11,22 +11,51 @@ import {Link} from "react-router-dom";
 import {ENDPOINT} from "../../../App";
 import {Transaction} from "../../../types/Transaction";
 
+import TableFooter from "@material-ui/core/TableFooter/TableFooter";
+import TablePagination from "@material-ui/core/TablePagination/TablePagination";
+import {tablePaginationActionsWrapped} from "../blocks/parts/TablePaginationActions";
+
+import ErrorForm from "../../errors/Error";
+import Loader from "../../loader/Loader";
+
 interface AddressState {
     address: string;
     loading: boolean;
     transactions: Transaction[];
-    error?: never;
+    error?: string;
+
+    page: number;
+    rowsPerPage: number;
 }
 
+const START_PAGE = 0;
+const DEFAULT_ROWS_PER_PAGE = 10;
+
 class AddressPage extends React.Component<any, AddressState> {
+    constructor(props: any) {
+        super(props);
+
+        this.handleChangePage = this.handleChangePage.bind(this);
+        this.handleChangeRowsPerPage = this.handleChangeRowsPerPage.bind(this);
+        this.loadTransactions = this.loadTransactions.bind(this);
+    }
+
     public state = {
         address: this.props.match.params.address,
         loading: false,
         transactions: [],
+        page: START_PAGE,
+        rowsPerPage: DEFAULT_ROWS_PER_PAGE,
     } as AddressState;
 
     public componentDidMount() {
-        const url = ENDPOINT + "/transactions?select=*&limit=15&or=(from.eq." + this.state.address + ",to.eq." + this.state.address + ")&order=nonce.desc";
+        this.loadTransactions();
+    }
+
+    public loadTransactions() {
+        const limit = this.state.rowsPerPage;
+        const offset = this.state.page * this.state.rowsPerPage;
+        const url = ENDPOINT + "/transactions?select=*&limit=" + limit + "&offset=" + offset + "&or=(from.eq." + this.state.address + ",to.eq." + this.state.address + ")&order=nonce.desc";
         console.log(url);
         fetch(url)
             .then((response) => {
@@ -49,8 +78,36 @@ class AddressPage extends React.Component<any, AddressState> {
             });
     }
 
+    public handleChangePage(event: any, page: number) {
+        this.setState({
+            page,
+            loading: false,
+        } as AddressState, this.loadTransactions);
+    }
+
+    public handleChangeRowsPerPage(event: any) {
+        this.setState({
+            rowsPerPage: event.target.value,
+            loading: false,
+        } as AddressState, this.loadTransactions);
+    }
+
     public render() {
-        console.log(this.state);
+        const {page, rowsPerPage} = this.state;
+
+        if (this.state.error != null) {
+            const err = this.state.error.toString();
+            return (
+                <ErrorForm error={err}/>
+            );
+        }
+
+        if (!this.state.loading) {
+            return (
+                <Loader/>
+            );
+        }
+
         return (
             <Grid>
                 <h1 style={{padding: 16}}>Address - {this.state.address}</h1>
@@ -83,6 +140,22 @@ class AddressPage extends React.Component<any, AddressState> {
                                 );
                             })}
                         </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TablePagination
+                                    colSpan={3}
+                                    count={2000000}
+                                    // TODO: load state before & in time:
+                                    // колличество транзакций будет постоянно меняться
+                                    // при использовании offset на страницу будут догружаться
+                                    rowsPerPage={rowsPerPage}
+                                    page={page}
+                                    onChangePage={this.handleChangePage}
+                                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                                    ActionsComponent={tablePaginationActionsWrapped}
+                                />
+                            </TableRow>
+                        </TableFooter>
                     </Table>
                 </Paper>
             </Grid>
