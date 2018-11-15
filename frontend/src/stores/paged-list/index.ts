5990d0af6d7a15d66c90aa4a68fe5e2060ca083f
maestrow <andrew.shmelev@gmail.com>
2018-11-15 18:18:17 +0300

    [front] unistore: Implement list actions as a class, thus it became possible to call one action from another;

--- frontend/src/stores/paged-list/index.ts
+++ frontend/src/stores/paged-list/index.ts
@@ -1,9 +1,9 @@
 import createStore, { Store } from 'unistore';
 import { IListState } from './types';
 
-type TFetch<TItem> = (page: number, pageSize: number) => Promise<TItem[]>;
+type TFetch<TItem> = (page: number, pageSize: number) => Promise<TItem[] | string>;
 
-const initStore = <TItem>(pageSize = 20, page = 1): Store<IListState<TItem>> => {
+const initStore = <TItem>(pageSize = 10, page = 1): Store<IListState<TItem>> => {
     const list: TItem[] = [];
     return createStore({
         list,
@@ -13,27 +13,42 @@ const initStore = <TItem>(pageSize = 20, page = 1): Store<IListState<TItem>> => 
     });
 };
 
-const initActions = <TItem>(fetch: TFetch<TItem>) =>
-    (store: Store<IListState<TItem>>) => ({
-        changePageSize: async (_: any, pageSize: number): Promise<void> => {
-            return new Promise<void>((resolve) => {
-                store.setState({ loading: true });
-                setTimeout(() => {
-                    store.setState({ loading: false });
-                    resolve();
-                }, 200);
-            });
-        },
-        fetch: async (state: IListState<TItem>, page: number) => {
-            store.setState({ loading: true });
-            const list: TItem[] = await fetch(page, state.pageSize);
-            store.setState({
-                loading: false,
-                page,
-                list,
-            });
-        },
-    });
+class Actions<TItem> {
+    constructor(
+        fetchMethod: TFetch<TItem>,
+        store: Store<IListState<TItem>>
+    ) {
+        this.fetchMethod = fetchMethod;
+        this.store = store;
+    }
+
+    private fetchMethod: TFetch<TItem>;
+    private store: Store<IListState<TItem>>;
+
+    public changePageSize = async (_: any, pageSize: number): Promise<void> => {
+        this.store.setState({ pageSize });
+        this.fetch(this.store.getState());
+    }
+
+    public fetch = async (state: IListState<TItem>, page?: number) => {
+        this.store.setState({ loading: true });
+        const p = page === undefined ? 1 : page;
+        const result = await this.fetchMethod(p, state.pageSize);
+        const upd = typeof(result) === 'string'
+            ? { error: undefined }
+            : { list: result };
+        const update: Pick<IListState<TItem>, 'loading' | 'page'> = {
+            loading: false,
+            page: p,
+            ...upd,
+        };
+        this.store.setState(update);
+    }
+}
+
+const initActions = <TItem>(fetchMethod: TFetch<TItem>) =>
+    (store: Store<IListState<TItem>>) =>
+    new Actions(fetchMethod, store);
 
 export default {
     initStore,
