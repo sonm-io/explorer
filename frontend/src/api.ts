621acfda07fe59df41f7de8365f908ec68dabb09
maestrow <andrew.shmelev@gmail.com>
2018-11-16 15:42:24 +0300

    [front] Extract API module. Make list fetch method generic.

---
+++ frontend/src/api.ts
@@ -0,0 +1,28 @@
+import { EndpointAddr } from 'src/config';
+
+interface IPageParams {
+    offset: number;
+    limit: number;
+}
+
+const list = (queryTplFactory: (pageParams: IPageParams) => string) =>
+    async (page: number, pageSize: number) => {
+        const offset = pageSize * page;
+        const limit = pageSize;
+        const query = queryTplFactory({ offset, limit });
+        const url = `${EndpointAddr}/${query}`;
+        console.log(url);
+        return await fetch(url)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error(res.statusText);
+                }
+                return res.json();
+            })
+            .catch((error) => {
+                return error.toString();
+            });
+    };
+
+export const blocks = list(({offset, limit}) =>
+    `blocks?order=number.desc&limit=${limit}&offset=${offset}`);
--- frontend/src/components/pages/blocks/index.ts
+++ frontend/src/components/pages/blocks/index.ts
@@ -1,22 +1,5 @@
 import { BlocksPage } from './BlocksPage';
-import { EndpointAddr } from 'src/config';
 import { createListPage } from 'src/components/list';
+import * as Api from 'src/api';
 
-const fetchData = async (page: number, pageSize: number) => {
-    const offset = pageSize * page;
-    const limit = pageSize;
-    const url = EndpointAddr + "/blocks?order=number.desc&limit=" + limit + "&offset=" + offset;
-    console.log(url);
-    return await fetch(url)
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error(res.statusText);
-            }
-            return res.json();
-        })
-        .catch((error) => {
-            return error.toString();
-        });
-};
-
-export default () => createListPage(BlocksPage, fetchData);
+export default () => createListPage(BlocksPage, Api.blocks);
