7f717f49ab338ae0cfaa9d626f9eedf90dbf5592
Anton Shchukin <szwtdy@gmail.com>
2018-12-05 14:41:20 +0300

    [back] ref: storage (#34)
    * [backend] aux: refactor db to storage
    
    * out: remove build from frontend target

---
+++ backend/storage/storage_test.go
@@ -0,0 +1,35 @@
+package storage
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const (
+	expectedBestBlock      uint64 = 1000
+	expectedIntervalsCount int    = 500
+)
+
+func testGetBestBlock(t *testing.T) {
+	bestBlock, err := testStorage.GetBestBlock()
+	assert.NoError(t, err, "GetBestBlock failed")
+	assert.Equal(t, expectedBestBlock, bestBlock,
+		"incorrect reply: expected %d, get %d", expectedBestBlock, bestBlock)
+}
+
+func testGetUnfilledIntervals(t *testing.T) {
+	_, err := testStorage.db.Exec("DELETE FROM blocks WHERE number % 2 != 0")
+	if err != nil {
+		t.Fatalf("failed to prepare test data %s \r\n", err)
+	}
+	_, err = testStorage.db.Exec(insertBlockQuery, 1100, "0xdddddddddddddddddd", "", 0, "", "", "", "", "", "", 0, 0, 0, "", 0, 0, 0, "", 0)
+	if err != nil {
+		t.Fatalf("failed to prepare test data %s \r\n", err)
+	}
+
+	intervals, err := testStorage.GetUnfilledIntervals()
+	assert.NoError(t, err, "GetUnfilledIntervals failed")
+	// GetUnfilledIntervals must returns only 500 unique intervals
+	assert.Equal(t, expectedIntervalsCount, len(intervals),
+		"incorrect returned intervals count: expected %d, get %d", expectedIntervalsCount, len(intervals))
+}
