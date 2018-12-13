c51eb27806c35d62f9f131f753588a279db5ddaa
maestrow <andrew.shmelev@gmail.com>
2018-12-13 19:55:47 +0300

    [front] fix: Add router to styleguidist wrapper to correctly display links.

--- frontend/src/styleguide/Wrapper.tsx
+++ frontend/src/styleguide/Wrapper.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import { theme } from "../theme";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { Router } from 'react-router';
+import createBrowserHistory from "history/createBrowserHistory";
+const history = createBrowserHistory();
 
 export default class Wrapper extends React.Component {
     public render() {
         return (
             <MuiThemeProvider theme={theme}>
-                {this.props.children}
+                <Router history={history}>
+                    {this.props.children}
+                </Router>
             </MuiThemeProvider>
         );
     }
