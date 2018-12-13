46de9381469c2f69c964c7853d73bd8e43e2377b
Andrey Shmelev <andrew.shmelev@gmail.com>
2018-12-13 15:35:36 +0300

    [front] DatetimePicker: fix bugs and make styleguidist sample (#45)

--- frontend/src/components/common/datetime-picker/index.tsx
+++ frontend/src/components/common/datetime-picker/index.tsx
@@ -18,25 +18,68 @@ type TDateTimePickerCss = 'container' | 'textField';
 
 export interface IDateTimePickerProps extends WithStyles<TDateTimePickerCss> {
     label?: string;
+    value?: Date;
+    onChange: (value?: Date) => void;
 }
 
-const DateTimePicker = (p: IDateTimePickerProps) => {
-    const { classes } = p;
-    return (
-        <form className={classes.container} noValidate>
-            <TextField
-                label={p.label}
-                type="datetime-local"
-                className={classes.textField}
-                InputLabelProps={{
-                    shrink: true,
-                }}
-                inputProps={{
-                    className: 'date-time-picker__input'
-                }}
-            />
-        </form>
-    );
+const inputLabelProps = {
+    shrink: true,
 };
+
+const inputProps = {
+    className: 'date-time-picker__input'
+};
+
+function isValidDate(d: any) {
+    return d instanceof Date && !isNaN(d as any);
+}
+
+class DateTimePicker extends React.Component<IDateTimePickerProps, never> {
+
+    constructor(props: IDateTimePickerProps) {
+        super(props);
+    }
+
+    private handleBlur = () => {
+        if (this.props.onChange !== undefined) {
+            const value = this.inputRef === undefined ? undefined : new Date(this.inputRef.value);
+            console.log('inputRef value [' + value + ']');
+            this.props.onChange(value);
+        }
+    }
+
+    private inputRef: any;
+
+    private saveInputRef = (ref: any) => {
+        this.inputRef = ref;
+    }
+
+    private dateAsString = () => {
+        const date = this.props.value;
+        return date === undefined || !isValidDate(date)
+            ? ''
+            : date.toISOString().substring(0, 16);
+    }
+
+    public render() {
+        console.log(this.dateAsString());
+        const { classes, label, value, onChange, ...p } = this.props;
+        return (
+            <form className={classes.container} noValidate>
+                <TextField
+                    inputRef={this.saveInputRef}
+                    label={label}
+                    type="datetime-local"
+                    className={classes.textField}
+                    InputLabelProps={inputLabelProps}
+                    inputProps={inputProps}
+                    onBlur={this.handleBlur}
+                    defaultValue={this.dateAsString()}
+                    {...p}
+                />
+            </form>
+        );
+    }
+}
 
 export default withStyles(styles)(DateTimePicker);
