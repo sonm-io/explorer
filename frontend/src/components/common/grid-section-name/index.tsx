29259c377a12c8ca6278b041bdaf107c875227eb
maestrow <andrew.shmelev@gmail.com>
2018-12-17 15:16:29 +0300

    [front] aux: Add classes to grid-label, grid-section-name, grid-value.

--- frontend/src/components/common/grid-section-name/index.tsx
+++ frontend/src/components/common/grid-section-name/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { Grid } from '@material-ui/core';
+import * as cn from 'classnames';
 import './grid-section-name.less';
 
 const GridSectionName = (props: any) => (
-    <Grid className="grid-section-name" item xs={12}>{props.children}</Grid>
+    <Grid className={cn('grid-section-name', props.className)} item xs={12}>{props.children}</Grid>
 );
 
 export default GridSectionName;
--- frontend/src/components/common/grid-value/index.tsx
+++ frontend/src/components/common/grid-value/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Grid } from '@material-ui/core';
 
 const GridValue = (props: any) => (
-    <Grid item xs={10}>{props.children}</Grid>
+    <Grid className={props.className} item xs={10}>{props.children}</Grid>
 );
 
 export default GridValue;
