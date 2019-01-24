060ecccd907858cceec8a37545d48595c6085f12
Anton Shchukin <szwtdy@gmail.com>
2019-01-24 17:31:56 +0300

    [front] feat: add status to transaction (#83)

--- frontend/src/components/pages/transaction/TransactionPage.tsx
+++ frontend/src/components/pages/transaction/TransactionPage.tsx
@@ -3,20 +3,21 @@ import * as React from "react";
 import Grid from "@material-ui/core/Grid/Grid";
 import {Link} from "src/components/common/link";
 import {Transaction} from "src/types/Transaction";
-import { IItemProps } from "src/components/factories/item";
+import {IItemProps} from "src/components/factories/item";
 import Header from "src/components/common/header";
 import SectionName from 'src/components/common/grid-section-name';
 import Label from 'src/components/common/grid-label';
 import Value from 'src/components/common/grid-value';
 
-interface ITransactionPageProps extends IItemProps<Transaction> {}
+interface ITransactionPageProps extends IItemProps<Transaction> {
+}
 
 export class TransactionPage extends React.Component<ITransactionPageProps, never> {
     private renderMain(item: Transaction) {
         return (
             <div className="transaction">
                 <div className="head-container">
-                    <Header title="Transaction details" />
+                    <Header title="Transaction details"/>
                 </div>
                 <Grid className="content-container" container spacing={16}>
                     <SectionName>Transaction info</SectionName>
@@ -29,6 +30,14 @@ export class TransactionPage extends React.Component<ITransactionPageProps, neve
                         <Link to={"/block/" + item.blockNumber}>
                             {item.blockNumber}
                         </Link>
+                    </Value>
+
+                    <Label>Status</Label>
+                    <Value>
+                        {item.status
+                            ? "Success"
+                            : "Failed"
+                        }
                     </Value>
 
                     <Label>From</Label>
