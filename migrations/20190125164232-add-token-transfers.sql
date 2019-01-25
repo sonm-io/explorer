992ddfcc57a596425c274764a04ef0cffd213480
Antony Shchukin <szwtdy@gmail.com>
2019-01-25 18:37:45 +0300

    [backend] fix: token transfer view

--- migrations/20190125164232-add-token-transfers.sql
+++ migrations/20190125164232-add-token-transfers.sql
@@ -3,18 +3,14 @@
 CREATE VIEW token_transfers AS
 SELECT t.hash,
        t."blockNumber",
-       t."from",
-       t."to",
-       l."firstTopic",
-       l."secondTopic",
-       l."thirdTopic",
-       l."firstArg",
-       l."secondArg",
-       l."thirdArg",
+       l."firstTopic" AS "method",
+       l."secondTopic" AS "from",
+       l."thirdTopic" AS "to",
+       l."firstArg" AS "value",
        b.timestamp
 FROM transactions t
-       INNER JOIN logs l on t.hash = l."txHash"
-       INNER JOIN blocks b on t."blockHash" = b.hash
+            INNER JOIN logs l on t.hash = l."txHash"
+            INNER JOIN blocks b on t."blockHash" = b.hash
 WHERE l."firstTopic" = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
 
 -- +migrate Down
