1b9d89064ff1c434b1ded103a25c6908ee608693
ALex Nikonov <alex@sshaman.ru>
2019-01-29 14:26:25 +0300

    [backend] refactor: make config great again (#97)
    * split geth and filler configs;
    * concurrency factor is now configurabe;
    * better naming, some docstrings;

--- backend/filler/config.go
+++ backend/filler/config.go
@@ -1,25 +1,47 @@
 package filler
 
 import (
+	"fmt"
 	"github.com/jinzhu/configor"
 	"github.com/sonm-io/explorer/backend/storage"
 )
 
 type fillerConfig struct {
+	Concurrency uint `yaml:"concurrency" required:"true" default:"50"`
+}
+
+type gethConfig struct {
 	Endpoint string `yaml:"endpoint" required:"true"`
 }
 
 type Config struct {
-	Filler   *fillerConfig   `yaml:"filler" required:"true"`
-	Database *storage.Config `yaml:"database" required:"true"`
+	Filler   fillerConfig   `yaml:"filler" required:"true"`
+	Geth     gethConfig     `yaml:"geth" required:"true"`
+	Database storage.Config `yaml:"database" required:"true"`
+}
+
+func (c *Config) validate() error {
+	if c.Filler.Concurrency == 0 {
+		return fmt.Errorf("cannot use zero concurrency")
+	}
+
+	if c.Filler.Concurrency >= 500 {
+		return fmt.Errorf("it's too danger to use so many threads")
+	}
+
+	return nil
 }
 
 func NewConfig(path string) (*Config, error) {
 	cfg := &Config{}
 
-	err := configor.Load(cfg, path)
-	if err != nil {
+	if err := configor.Load(cfg, path); err != nil {
 		return nil, err
 	}
+
+	if err := cfg.validate(); err != nil {
+		return nil, err
+	}
+
 	return cfg, nil
 }
